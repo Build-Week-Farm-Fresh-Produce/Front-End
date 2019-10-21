a7b513323e3ae4c708193c0e298345d2a07489b2
Afia-Lydia <you@example.com>
2019-10-21 19:10:46 -0400

    updated login and signup pages

--- farm-fresh/src/App.js
+++ farm-fresh/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import SignIn from './components/SignIn'
+import Registration from './components/Registration'
+import Home from './components/Home'
+
+
+import PrivateRoute from './utils/PrivateRoute'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path='/signin' component={SignIn} />
+      <Route path='/register' component={Registration} />
+      <Route exact path='/' component={Home} />
+    </Router>
+
   );
 }
 
